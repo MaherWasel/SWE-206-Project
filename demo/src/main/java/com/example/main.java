692347ee9527a82c39509f0ab79d6d0d7186e782
MaherWasel <132270545+MaherWasel@users.noreply.github.com>
2023-05-01 18:21:58 +0300

    TEST

---
+++ demo/src/main/java/com/example/main.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import javafx.application.Application;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class main extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
